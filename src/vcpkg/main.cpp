baf5bbeced121b88cd1f52ae05b798814b6caa0c
Basvas j.k.j <basvas@seznam.cz>
2026-02-10 18:55:25 +0100

    Add vcpkg example.

---
+++ src/vcpkg/main.cpp
@@ -0,0 +1,17 @@
+import std;
+import <nlohmann/json.hpp>;
+
+using namespace std;
+using namespace nlohmann;
+
+int main()
+{
+	json root;
+	root["a"] = 1;
+	root["b"].push_back(2);
+	root["b"].push_back(3);
+	root["c"]["d"] = 4;
+	root["c"]["e"] = 5;
+
+	print("{}", root.dump());
+}
